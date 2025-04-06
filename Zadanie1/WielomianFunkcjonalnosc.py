c8e828d94383eed9780961c0be84f783de48f8c2
MatiJens <mateuszjens14@gmail.com>
2025-04-06 14:50:46 +0200

    start pisania testow

--- Zadanie1/WielomianFunkcjonalnosc.py
+++ Zadanie1/WielomianFunkcjonalnosc.py
@@ -1,11 +1,11 @@
 from Zadanie1.Wielomian import Wielomian
 
-wielomianJeden = Wielomian([1,2,3,2.5,3])
-wielomianDwa = Wielomian([2,3,4,4,2,2])
-wielomianJedenSub = Wielomian([1,2,3,2.5,3])
-wielomianDwaSub = Wielomian([2,3,4,4,2,2])
-wielomianJedenMul = Wielomian([1,2,3,2.5,3])
-wielomianDwaMul = Wielomian([2,3,4,4,2,2])
+wielomianJeden = Wielomian([1,2,3])
+wielomianDwa = Wielomian([3,4,5,6])
+wielomianJedenSub = Wielomian([1,2,3])
+wielomianDwaSub = Wielomian([3,4,5,6])
+wielomianJedenMul = Wielomian([1,2,3])
+wielomianDwaMul = Wielomian([3,4,5,6])
 
 print(wielomianJeden.stopienWielomianu())
 print(wielomianJeden)
--- Zadanie1/test_Wielomian.py
+++ Zadanie1/test_Wielomian.py
@@ -1,6 +1,10 @@
-from unittest import TestCase
+from Zadanie1.Wielomian import Wielomian
 
 
-class TestWielomian(TestCase):
-    def test_stopien_wielomianu(self):
-        self.fail()
+def test_stopien_wielomianu():
+    w1 = Wielomian([1,2,3])
+    w2 = Wielomian([3,4,5,6])
+    assert w1.stopienWielomianu() == 2
+    assert w1.__str__() == "W(x) = 3x^2 + 2x^1 + 1"
+    assert w1(3) == 34
+    assert w1 + w2 ==

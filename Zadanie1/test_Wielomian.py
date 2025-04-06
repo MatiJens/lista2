d80d7d632f11425947821da0c866be4edca8d14a
MatiJens <mateuszjens14@gmail.com>
2025-04-06 16:10:15 +0200

    testy zadanie 1

--- Zadanie1/test_Wielomian.py
+++ Zadanie1/test_Wielomian.py
@@ -1,10 +1,19 @@
+import pytest
+
 from Zadanie1.Wielomian import Wielomian
 
 
 def test_stopien_wielomianu():
-    w1 = Wielomian([1,2,3])
-    w2 = Wielomian([3,4,5,6])
-    assert w1.stopienWielomianu() == 2
-    assert w1.__str__() == "W(x) = 3x^2 + 2x^1 + 1"
-    assert w1(3) == 34
-    assert w1 + w2 ==
+    assert Wielomian([1,2,3]).stopienWielomianu() == 2
+    assert Wielomian([1,2,3]).__str__() == "W(x) = 3x^2 + 2x^1 + 1"
+    assert Wielomian([1,2,3])(3) == 34
+    assert str(Wielomian([1,2,3]) + Wielomian([3,4,5,6])) == "W(x) = 6x^3 + 8x^2 + 6x^1 + 4"
+    assert str(Wielomian([1,2,3]).__iadd__(Wielomian([3,4,5,6]))) == "W(x) = 6x^3 + 8x^2 + 6x^1 + 4"
+    assert str(Wielomian([1,2,3]) - Wielomian([3,4,5,6])) == "W(x) = -6x^3 + -2x^2 + -2x^1 + -2"
+    assert str(Wielomian([1,2,3]).__isub__(Wielomian([3,4,5,6]))) == "W(x) = -6x^3 + -2x^2 + -2x^1 + -2"
+    assert str(Wielomian([1,2,3]) * Wielomian([3,4,5,6])) == "W(x) = 15x^2 + 8x^1 + 3"
+    assert str(Wielomian([1,2,3]).__imul__(Wielomian([3,4,5,6]))) == "W(x) = 15x^2 + 8x^1 + 3"
+    with pytest.raises(TypeError):
+        Wielomian("[1,2,3]")
+    with pytest.raises(TypeError):
+        Wielomian([1,2,3])("3")
